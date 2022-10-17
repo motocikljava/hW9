41f063814f349369668daa9bdb25be43fdddbe88
Anton <fynjycedjhjd@gmail.com>
2022-10-17 20:48:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // задание 1
+        String firstName = "Ivan" + " ";
+        String middleName = "Ivanovich" + " ";
+        String lastName = "Ivanov" + " ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        // задание 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+//        Задание 3
+//        String firstName = "Семён" + " ";
+//        String middleName = "Семёнович" + " ";
+//        String lastName = "Иванов" + " ";
+//        String fullName = lastName + firstName + middleName;
+//        fullName = fullName.replace("ё", "е");
+//        System.out.println("ФИО сотрудника — " + fullName);
+    }
+}
